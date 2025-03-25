d28ead1b60af04ed8849c09275e6ccacd79fd321
Mohamed Salah <mosayed26303@gmail.com>
2025-03-25 23:53:09 +0200

    Add project files.

---
+++ Infrastructure/Presestance/Data/Configurations/ProductConfig.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    internal class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            #region Product
+            builder.Property(product => product.Price)
+                .HasColumnType("decimal(18,3)");
+            #endregion
+            #region ProductBrand
+            builder.HasOne(product => product.productBrand)
+                .WithMany().HasForeignKey(product => product.BrandId);
+            #endregion
+            #region ProductType
+            builder.HasOne(product => product.productType)
+                .WithMany().HasForeignKey(product => product.TypeId);
+            #endregion
+        }
+    }
+}
